2cb40496a148ba43c9c7b3eb40960b19ff77c30c
inaba <numberg0311@gmail.com>
2026-01-13 14:36:24 +0900

    add: Jest + jest-cucumber テスト基盤
    - what: jest.config.js を追加。ts-jest による TypeScript 対応、jest-cucumber による BDD テスト対応を設定
    - why: packages/apps 配下のユニットテストと BDD テストを統一的に実行可能にする

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/packages', '<rootDir>/apps'],
+  testMatch: ['**/*.steps.ts', '**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'feature'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+        isolatedModules: false
+      }
+    ]
+  },
+  moduleNameMapper: {
+    '^@podcast_search/config$': '<rootDir>/packages/config/src/index.ts',
+    '^@podcast_search/database$': '<rootDir>/packages/database/src/index.ts',
+    '^@podcast_search/search-core$': '<rootDir>/packages/search-core/src/index.ts'
+  },
+  collectCoverageFrom: ['packages/*/src/**/*.ts', 'apps/*/src/**/*.ts', '!**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  verbose: true
+}
